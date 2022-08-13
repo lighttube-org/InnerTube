79297c25564332ed897cff1a61d772266bd9c26a
Kayra Uylar <k.uylar@outlook.com>
2022-08-13 15:10:57 +0300

    Make sure that the authentication is working

---
+++ InnerTube.Tests/AuthenticationTests.cs
@@ -0,0 +1,33 @@
+﻿namespace InnerTube.Tests;
+
+public class AuthenticationTests
+{
+	[Test]
+	public async Task GetAuthorizedPlayerWithSapisid()
+	{
+		InnerTube tube = new(InnerTubeAuthorization.SapisidAuthorization(
+			Environment.GetEnvironmentVariable("INNERTUBE_SAPISID") ??
+			throw new ArgumentNullException("INNERTUBE_SAPISID",
+				"Please set the INNERTUBE_SAPISID environment variable."),
+			Environment.GetEnvironmentVariable("INNERTUBE_PSID") ??
+			throw new ArgumentNullException("INNERTUBE_PSID",
+				"Please set the INNERTUBE_PSID environment variable.")
+		));
+
+		InnerTubePlayer player = await tube.GetPlayerAsync("V6kJKxvbgZ0", true, false);
+		Assert.Pass($"Received player with {player.Formats.Count()} muxed formats & {player.AdaptiveFormats.Count()} muxed formats");
+	}
+
+	[Test]
+	public async Task GetAuthorizedPlayerWithRefreshToken()
+	{
+		InnerTube tube = new(InnerTubeAuthorization.RefreshTokenAuthorization(
+			Environment.GetEnvironmentVariable("INNERTUBE_REFRESH_TOKEN") ??
+			throw new ArgumentNullException("INNERTUBE_REFRESH_TOKEN",
+				"Please set the INNERTUBE_REFRESH_TOKEN environment variable.")
+		));
+
+		InnerTubePlayer player = await tube.GetPlayerAsync("V6kJKxvbgZ0", true, false);
+		Assert.Pass($"Received player with {player.Formats.Count()} muxed formats & {player.AdaptiveFormats.Count()} muxed formats");
+	}
+}
