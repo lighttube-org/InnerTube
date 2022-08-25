b9d16a21f092a7484896a5ad6f77463ed2f8f0af
Kayra Uylar <k.uylar@outlook.com>
2022-08-25 17:55:53 +0300

    Add Icon and Tooltip property to the Badge

--- InnerTube/Models/Badge.cs
+++ InnerTube/Models/Badge.cs
@@ -5,13 +5,17 @@ namespace InnerTube;
 public class Badge
 {
 	public string Style { get; }
-	public string Label { get; }
+	public string? Label { get; }
+	public string? Tooltip { get; }
+	public string? Icon { get; }
 
 	public Badge(JToken metadataBadgeRenderer)
 	{
-		Style = metadataBadgeRenderer["style"]!.ToString()!;
-		Label = metadataBadgeRenderer["label"]!.ToString()!;
+		Style = metadataBadgeRenderer["style"]!.ToString();
+		Label = metadataBadgeRenderer["label"]?.ToString();
+		Tooltip = metadataBadgeRenderer["tooltip"]?.ToString();
+		Icon = metadataBadgeRenderer["icon"]?["iconType"]?.ToString();
 	}
 
-	public override string ToString() => $"[{Style}] {Label}";
+	public override string ToString() => $"[{Style}]{(Icon is not null ? $" ({Icon})" : "")} {(Label is not null ? $", {Tooltip}" : "(no label)")}{(Tooltip is not null ? $", {Tooltip}" : "")}";
 }
