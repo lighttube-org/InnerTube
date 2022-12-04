4476692a764dc2b4cc91f7709c47edf9c098ce67
Kayra Uylar <k.uylar@outlook.com>
2022-12-04 21:32:27 +0300

    Make the HtmlFormatter escape < and >

--- InnerTube/Formatters/HtmlFormatter.cs
+++ InnerTube/Formatters/HtmlFormatter.cs
@@ -2,10 +2,14 @@
 
 public class HtmlFormatter : IFormatter
 {
-	public string FormatBold(string text) => $"<b>{text}</b>";
+	public string FormatBold(string text) => $"<b>{Sanitize(text)}</b>";
 
-	public string FormatItalics(string text) => $"<i>{text}</i>";
+	public string FormatItalics(string text) => $"<i>{Sanitize(text)}</i>";
 
-	public string FormatUrl(string text, string url) => $"<a href=\"{url}\">{text}</a>";
+	public string FormatUrl(string text, string url) => $"<a href=\"{url}\">{Sanitize(text)}</a>";
 	public string HandleLineBreaks(string text) => text.Replace("\n", "<br>");
+
+	private string Sanitize(string text) => text
+		.Replace("<", "&lt;")
+		.Replace(">", "&gt;");
 }
