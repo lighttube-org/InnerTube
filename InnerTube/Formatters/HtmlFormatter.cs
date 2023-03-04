489f87c8fa97687b248dcf922fdc00ee930e935f
Kayra Uylar <k.uylar@outlook.com>
2023-03-04 21:22:43 +0300

    Add XMLDocs for formatters

--- InnerTube/Formatters/HtmlFormatter.cs
+++ InnerTube/Formatters/HtmlFormatter.cs
@@ -1,12 +1,20 @@
 ﻿namespace InnerTube.Formatters;
 
+/// <summary>
+/// Formatter used to output rich texts in HTML format
+/// </summary>
 public class HtmlFormatter : IFormatter
 {
+	/// <inheritdoc />
 	public string FormatBold(string text) => $"<b>{Sanitize(text)}</b>";
 
+	/// <inheritdoc />
 	public string FormatItalics(string text) => $"<i>{Sanitize(text)}</i>";
 
+	/// <inheritdoc />
 	public string FormatUrl(string text, string url) => $"<a href=\"{url}\">{Sanitize(text)}</a>";
+
+	/// <inheritdoc />
 	public string HandleLineBreaks(string text) => text.Replace("\n", "<br>");
 
 	private string Sanitize(string text) => text
--- InnerTube/Formatters/MarkdownFormatter.cs
+++ InnerTube/Formatters/MarkdownFormatter.cs
@@ -1,12 +1,19 @@
 ﻿namespace InnerTube.Formatters;
 
+/// <summary>
+/// Formatter used to output rich texts in markdown format
+/// </summary>
 public class MarkdownFormatter : IFormatter
 {
+	/// <inheritdoc />
 	public string FormatBold(string text) => $"**{text}**";
 
+	/// <inheritdoc />
 	public string FormatItalics(string text) => $"_{text}_";
 
+	/// <inheritdoc />
 	public string FormatUrl(string text, string url) => $"[{text}]({url})";
 
+	/// <inheritdoc />
 	public string HandleLineBreaks(string text) => text;
 }
