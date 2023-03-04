129c6f808497cc51b8132ec889000ab96fa572e0
Kayra Uylar <k.uylar@outlook.com>
2023-03-04 21:15:52 +0300

    Add XMLDocs for exceptions

--- InnerTube/Exceptions/InnerTubeException.cs
+++ InnerTube/Exceptions/InnerTubeException.cs
@@ -1,5 +1,8 @@
 ﻿namespace InnerTube.Exceptions;
 
+/// <summary>
+/// An InnerTube-related exception.
+/// </summary>
 public class InnerTubeException : Exception
 {
 	internal InnerTubeException(string message) : base(message)
--- InnerTube/Exceptions/PlayerException.cs
+++ InnerTube/Exceptions/PlayerException.cs
@@ -1,12 +1,24 @@
 ﻿namespace InnerTube.Exceptions;
 
+/// <summary>
+/// Thrown when the received player response isn't playable
+/// </summary>
 public class PlayerException : InnerTubeException
 {
+	/// <summary>
+	/// Code of the error, from YouTube
+	/// </summary>
 	public string Code { get; }
+	/// <summary>
+	/// Error message, from YouTube
+	/// </summary>
 	public string Error { get; }
+	/// <summary>
+	/// Error description, from YouTube
+	/// </summary>
 	public string Description { get; }
 
-	public PlayerException(string code, string error, string description) : base($"[{code}] {error}")
+	internal PlayerException(string code, string error, string description) : base($"[{code}] {error}")
 	{
 		Code = code;
 		Error = error;
