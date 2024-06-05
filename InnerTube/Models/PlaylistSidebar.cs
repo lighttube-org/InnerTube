3f2f4823fe33d8af3435c1d032419a168c63b95d
Kayra Uylar <k.uylar@outlook.com>
2024-06-05 14:04:40 +0300

    Add lastUpdated to playlistSidebar

--- InnerTube/Models/PlaylistSidebar.cs
+++ InnerTube/Models/PlaylistSidebar.cs
@@ -14,7 +14,10 @@ public class PlaylistSidebar(PlaylistHeaderRenderer sidebar, string parserLangua
 	public long VideoCount { get; } = ValueParser.ParseVideoCount(parserLanguage, Utils.ReadRuns(sidebar.NumVideosText));
 	public string ViewCountText { get; } = Utils.ReadRuns(sidebar.ViewCountText);
 	public long ViewCount { get; } = ValueParser.ParseViewCount(parserLanguage, Utils.ReadRuns(sidebar.ViewCountText));
-	public string LastUpdated { get; } = Utils.ReadRuns(sidebar.Byline.PlaylistBylineRenderer.Text.Last());
+	public string LastUpdatedText { get; } = Utils.ReadRuns(sidebar.Byline.PlaylistBylineRenderer.Text.Last());
+
+	public DateTimeOffset LastUpdated { get; } = ValueParser.ParseLastUpdated(parserLanguage,
+		Utils.ReadRuns(sidebar.Byline.PlaylistBylineRenderer.Text.Last()));
 	public string Description { get; } = Utils.ReadRuns(sidebar.DescriptionText);
 	public Channel? Channel { get; } = Channel.From(sidebar.OwnerText);
 }
