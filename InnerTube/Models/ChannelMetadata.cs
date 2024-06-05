adf4c5132d54870a064448d0dec92dd526c1fb39
Kayra Uylar <k.uylar@outlook.com>
2024-06-05 14:06:12 +0300

    Fix the handle & description

--- InnerTube/Models/ChannelMetadata.cs
+++ InnerTube/Models/ChannelMetadata.cs
@@ -6,8 +6,8 @@ public class ChannelMetadata(ChannelMetadataRenderer metadata)
 {
 	public string Id { get; set; } = metadata.ExternalId;
 	public string Title { get; set; } = metadata.Title;
-	public string Description { get; set; } = metadata.Description;
-	public string Handle { get; set; } = metadata.VanityChannelUrl;
+	public string? Description { get; set; } = metadata.Description.NullIfEmpty();
+	public string? Handle { get; set; } = Channel.TryGetHandle(metadata.VanityChannelUrl);
 	public string RssUrl { get; set; } = metadata.RssUrl;
 	public string ChannelUrl { get; set; } = metadata.ChannelUrl;
 	public string Keywords { get; set; } = metadata.Keywords;
